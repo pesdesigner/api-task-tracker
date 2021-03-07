9cffef6c6b8c6c83a3134ae8ad432998b1e8fc47
pesdesigner <pauloeduardo.dg@outlook.com>
2021-03-07 12:54:08 -0300

    Domain model

---
+++ backend/src/main/java/com/graficoeweb/tasktracker/entities/Tasks.java
@@ -0,0 +1,93 @@
+package com.graficoeweb.tasktracker.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tasks")
+public class Tasks implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String text;
+	private String day;
+	private boolean reminder;
+	
+	public Tasks() {
+	}
+
+	public Tasks(Long id, String text, String day, boolean reminder) {
+		super();
+		this.id = id;
+		this.text = text;
+		this.day = day;
+		this.reminder = reminder;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public String getDay() {
+		return day;
+	}
+
+	public void setDay(String day) {
+		this.day = day;
+	}
+
+	public boolean getReminder() {
+		return reminder;
+	}
+
+	public void setReminder(boolean reminder) {
+		this.reminder = reminder;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Tasks other = (Tasks) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
+}
